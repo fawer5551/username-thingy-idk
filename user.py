d39c4363ffe78996fff19342587674f3b191fb42
fawer5551 <89085353+fawer5551@users.noreply.github.com>
2025-08-04 22:09:30 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,27 +1,28 @@
 import time
 
 while True:
-username = input("what would you like your username to be?")
-time.sleep(3)
-print("processing...")
-time.sleep(3)
-print("processing..")
-time.sleep(3)
-print("processing.")
-time.sleep(3)
-confirm = input(f"to confirm, you want your username to be '{username}' correct? (yes/no): ")
-if confirm.lower() == "yes":
- password = input("what would you like your password to be?")
- passconf = input("enter password again..")
- if passconf == password:
-  while True:
-  print("password confirmed, please wait..")
-  time.sleep(3)
-  print("Account succesfully created!")
- else
-  print("account has not been created.")
- break
-break
+    username = input("What would you like your username to be? ")
+    time.sleep(3)
+    print("processing...")
+    time.sleep(3)
+    print("processing..")
+    time.sleep(3)
+    print("processing.")
+    time.sleep(3)
 
-else:
-  print("okay, lets try again\n")
+    confirm = input(f"To confirm, you want your username to be '{username}' correct? (yes/no): ")
+    
+    if confirm.lower() == "yes":
+        password = input("What would you like your password to be? ")
+        passconf = input("Enter password again: ")
+
+        if passconf == password:
+            print("Password confirmed, please wait...")
+            time.sleep(3)
+            print("Account successfully created!")
+            break
+        else:
+            print("Passwords do not match. Account has not been created.")
+            break
+    else:
+        print("Okay, let's try again.\n")
