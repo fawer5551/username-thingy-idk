a022844a1840196956707e878c2886c41bf028f1
fawer5551 <89085353+fawer5551@users.noreply.github.com>
2025-08-04 21:37:34 +0300

    Update user.py

--- user.py
+++ user.py
@@ -5,10 +5,22 @@ username = input("what would you like your username to be?")
 time.sleep(3)
 print("processing...")
 time.sleep(3)
+print("processing..")
+time.sleep(3)
+print("processing.")
+time.sleep(3)
 confirm = input(f"to confirm, you want your username to be '{username}' correct? (yes/no): ")
 if confirm.lower() == "yes":
  password = input("what would you like your password to be?")
+ passconf = input("enter password again..")
+ if passconf == password:
+  while True:
+  print("password confirmed, please wait..")
+  time.sleep(3)
   print("Account succesfully created!")
+ else
+  print("account has not been created.")
+ break
 break
 
 else:
